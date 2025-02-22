f86e31782c4a8374f3742cd4fe47195ff7ad6482
dtris <daohuutri04@gmail.com>
2025-02-22 16:16:15 +0700

    add new user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,26 @@
+const conection = require("../config/database");
+
 const getHomepage = (req, res) => {
-  res.send("Hello World!");
+  res.render("home.ejs");
 };
 const getABC = (req, res) => {
   res.render("sample.ejs");
 };
+const postCreateUser = (req, res) => {
+  let email = req.body.email;
+  let name = req.body.myName;
+  let city = req.body.city;
+  conection.query(
+    `INSERT INTO Users(email, name, city) VALUES(?,?,?)`,
+    [email, name, city],
+    function (err, result) {
+      console.log("email: ", email, "name: ", name, "city: ", city);
+      res.send("user is created");
+    }
+  );
+};
 module.exports = {
   getHomepage,
   getABC,
+  postCreateUser,
 };
--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
 const app = express();
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const conection = require("./config/database");
 const port = process.env.PORT || 8081;
 const hostname = process.env.HOST_NAME;
-
+//config request.body
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 configViewEngine(app);
 app.use("/", webRoutes);
+conection.query(`select * from Users`, function (err, result, fields) {
+  console.log("check result>>", result);
+});
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
